863bba7cfa28468dc95c97ba7b77ed16460bf15e
kaleo22 <leonard.kaempf@icloud.com>
2025-04-09 19:34:49 +0200

    removed Trailing Whitespaces. Video Inference with Bounding Boxes and Annotations

--- test.py
+++ test.py
@@ -17,28 +17,6 @@ def get_args_parser():
     parser.add_argument("--end_frame", type=int, default=None, help="End frame for image processing")
     return parser
 
-def combine_all_masks(image_np, masks):
-    # Sicherstellen, dass alle Masken NumPy-Arrays sind
-    masks = [np.array(mask) for mask in masks]
-
-    # Erstelle eine leere Maske mit der gleichen Form wie das Eingangsbild
-    combined_mask = np.zeros_like(image_np)
-
-    # Iteriere über alle Masken und kombiniere sie
-    for mask in masks:
-        # Erweitere die Maske auf 3 Kanäle (RGB)
-        mask_3d = np.stack([mask] * 3, axis=-1)
-
-        # Wähle eine Farbe für die Maske (z. B. Rot)
-        mask_color = [255, 0, 0]
-
-        # Überlagere die Maske auf das kombinierte Bild
-        combined_mask = np.where(mask_3d > 0, mask_color, combined_mask)
-
-    # Kombiniere die Maske mit dem Eingangsbild
-    overlay = np.where(combined_mask > 0, combined_mask, image_np)
-
-    return overlay
 
 def ImageInference(image_pil, text, start_frame, end_frame, model):
     if start_frame != 0 and end_frame is not None:
@@ -52,11 +30,11 @@ def ImageInference(image_pil, text, start_frame, end_frame, model):
                 results = model.predict([image_pil], [text])
                 first_result = results[0]
                 masks = first_result["masks"]
-                try: 
+                try:
                     if not masks:
                         overlay = image_np
                         print("No masks found for the given text prompt.")
-                    
+
                 except:
                     #overlay = combine_all_masks(image_np, masks)
                     overlay = utils.draw_image(image_np, masks, first_result["mask_boxes"], first_result["mask_scores"], first_result["mask_labels"])
@@ -64,13 +42,13 @@ def ImageInference(image_pil, text, start_frame, end_frame, model):
                     plt.imsave(output_path, overlay.astype(np.uint8))
                     print(f"Processed image saved to {output_path}")
                 id += 1
-                
+
 
 
     else:
         image_np = np.array(image_pil)
         results = model.predict([image_pil], [text])
-        
+
 
         # Alle Keys aus Ergebnissen extrahieren
         first_result = results[0]
@@ -78,13 +56,12 @@ def ImageInference(image_pil, text, start_frame, end_frame, model):
         probs = first_result["scores"]
         labels = first_result["labels"]
         xyxy = first_result["boxes"]
-          
-        try: 
+
+        try:
             if not masks:
                 overlay = image_np
                 print("No masks found for the given text prompt.")
         except:
-            #overlay = combine_all_masks(image_np, masks)
             overlay = utils.draw_image(image_np, masks, xyxy, probs, labels)
         return overlay
 
@@ -111,16 +88,16 @@ def VideoInference(video_path, text_prompt, output_path, model):
             image_pil = Image.fromarray(frame, mode='L')
         else:
             image_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        
+
         with torch.no_grad():
             overlay = ImageInference(image_pil, text_prompt, start_frame, end_frame, model)
-        
+
         overlay_bgr = cv2.cvtColor(overlay.astype(np.uint8), cv2.COLOR_RGB2BGR)
-        
+
         out.write(overlay_bgr)
         del overlay_bgr, overlay, image_pil
 
-        
+
         if count < 10:
             count += 1
         else:
@@ -146,14 +123,14 @@ if __name__ == "__main__":
     start_frame = args.start_frame
     end_frame = args.end_frame
     model = LangSAM()
-    
+
     #Entscheidung Bild oder Video
     if video_path:
         output_path = f"{output_path}/output_video.mp4"
     else:
         output_path = f"{output_path}/output_image.png"
-    
-    #Entscheidung über Inferencemethode 
+
+    #Entscheidung über Inferencemethode
     if video_path:
         VideoInference(video_path, text_prompt, output_path, model)
 
